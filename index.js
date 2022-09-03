d91880f4ea511b4759dadb966ab04ec9d9b38d89
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-09-03 15:54:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,55 +11,6 @@ require('./server/config/express')(app);
 require('./server/config/mongoose');
 app.use(auth())
 
-const io = require("socket.io")(http, {
-   cors: {
-       origin: ["https://notexchange.shop/"],
-      credentials: true
-   }
-});
-const ChatRoom = require('./server/models/ChatRoom')
-
-   io.on('connection', async function (socket) {
-   
-  
-  let asd = await ChatRoom.find().populate("buyer").populate("seller");
-
- socket.emit('output', asd);
-
- socket.on('input', function(data){
-    console.log(data)
-});
-});
-
-
-  io.on('input', async function (data) {
-   console.log(data)
-
-    let chat = await ChatRoom.findById(chatId);
-   console.log(chat)
-   chat.insert({name: name, message: message}, function(){
-       io.emit('output', [data]);
-
-     
-      sendStatus({
-          message: 'Message sent',
-          clear: true
-     });
-   });
-
-});
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./client/build'));
-    
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
-    });
-} else {
-    app.get("/*", (req, res) => {
-        res.send("DB is running")
-    })
-}
 
 
 app.use(routes);
